575adc73306477386e1a3ad222432dd19d11fe93
cy-kim <kimseongyeun@gmail.com>
2022-11-09 10:34:22 -0500

    q2p2

--- 2021_D02/main.js
+++ 2021_D02/main.js
@@ -1,27 +1,54 @@
-const fs = require("fs")
+const fs = require("fs");
+const { validateRequest } = require("twilio/lib/webhooks/webhooks");
 
 const path = "2021_D02/input.txt";
 const entries = fs.readFileSync(path).toString().split("\n");
 
-let depth = 0
-let forward = 0
-
-entries.forEach(element => {
-    let [keyword, value] = element.split(" ")
-    value = Number(value)
+const solution1 = () => {
+  let depth = 0;
+  let forward = 0;
+  entries.forEach((element) => {
+    let [keyword, value] = element.split(" ");
+    value = Number(value);
     switch (keyword) {
-        case "forward":
-            forward += value
-            break
-        case "down":
-            depth += value
-            break
-        case "up":
-            depth -= value
-            break
-        default:
-            throw new Error(`${keyword} is not good oops`)
+      case "forward":
+        forward += value;
+        break;
+      case "down":
+        depth += value;
+        break;
+      case "up":
+        depth -= value;
+        break;
+      default:
+        throw new Error(`${keyword} is not good oops`);
     }
-});
+  });
+};
 
-console.log(depth * forward)
+const solution2 = (entries) => {
+  let depth = 0;
+  let forward = 0;
+  let aim = 0;
+  for (let entry of entries) {
+    let [keyword, value] = entry.split(" ");
+    value = Number(value);
+    switch (keyword) {
+      case "forward":
+        forward += value;
+        depth += value * aim;
+        break;
+      case "down":
+        aim += value;
+        break;
+      case "up":
+        aim -= value;
+        break;
+      default:
+        throw new Error(`${(keyword, value)} is incorrect ????`);
+    }
+  }
+  console.log(forward * depth);
+};
+
+solution2(entries);
