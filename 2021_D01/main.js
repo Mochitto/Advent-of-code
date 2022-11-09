d8fe01276df50b337c7b793988f0c4910243855c
cy-kim <kimseongyeun@gmail.com>
2022-11-09 09:44:33 -0500

    feat: p2 solution

--- 2021_D01/main.js
+++ 2021_D01/main.js
@@ -1,10 +1,37 @@
-const fs = require("fs")
+const fs = require("fs");
 
-let total = 0
-const path = "2021_D01/input.txt"
-const entries = fs.readFileSync(path).toString().split('\n'); 
+let total = 0;
+const path = "input.txt";
+const entries = fs.readFileSync(path).toString().split("\n");
 
-let increments = entries.reduce((previous, current) => {
-    Number(current) > Number(previous) ? total += 1 : null
-    return current
-})
+// console.log(entries);
+// let increments = entries.reduce((previous, current) => {
+//   Number(current) > Number(previous) ? (total += 1) : null;
+//   return current;
+// });
+
+let groupsOfThree = entries.reduce((prev, current, index, array) => {
+  if (index <= 1) {
+    return;
+  }
+  const prevNum = Number(array[index - 1]) || 0;
+  const prevPrevNum = Number(array[index - 2]) || 0;
+  const tripleGroup = prevNum + prevPrevNum + Number(current);
+  if (prev < tripleGroup && prev) {
+    total++;
+  }
+
+  console.log(
+    index,
+    "|",
+    prevPrevNum,
+    prevNum,
+    current,
+    "triple",
+    tripleGroup,
+    "total",
+    total
+  );
+  return tripleGroup;
+});
+console.log(total);
